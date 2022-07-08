02c65b9134c91cf0f667a9c87619fe24906dd2c9
GitHub Action <action@github.com>
2022-07-08 11:40:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/manuelnttdata/androidtemplates/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.manuelnttdata.androidtemplates.services
+
+import com.github.manuelnttdata.androidtemplates.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/manuelnttdata/androidtemplates/services/MyProjectService.kt
+++ src/main/kotlin/com/github/manuelnttdata/androidtemplates/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.manuelnttdata.androidtemplates.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.manuelnttdata.androidtemplates.MyBundle
 
 class MyProjectService(project: Project) {
 
